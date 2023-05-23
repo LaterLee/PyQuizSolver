3d3e0b0fb27188ea611425c16e1cfd59ffc75aaa
LaterLee Jay <3269488364@qq.com>
2023-05-23 11:13:29 +0000

    py_quiz_solve

--- quiz_10.py
+++ quiz_10.py
@@ -1,7 +1,9 @@
+# -*- coding: utf-8 -*-
 # 题目十:108、python中读取Excel文件的方法
 import pandas as pd
-f = open('example.xlsx')
-data = f.read()
-df = pd.read_excel()
-print(data)
-f.close()
+#f = open('example.xlsx')
+#data = f.read()
+#print(data)
+#f.close()
+df = pd.read_excel('example.xlsx')
+print(df)
